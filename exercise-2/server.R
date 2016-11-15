6e964b8e93b73fa4f8647bdc93a0a1fabb8cbd2d
addeck <addeck@uw.edu>
2016-11-15 13:47:37 -0800

    done exercise 2

--- exercise-2/server.R
+++ exercise-2/server.R
@@ -1,5 +1,5 @@
 ### Exercise 2 ###
-
+#install.packages('shiny')
 library(shiny)
 
 # We'll look into these more next week: http://shiny.rstudio.com/gallery/widget-gallery.html 
@@ -10,5 +10,6 @@ library(shiny)
 # It should return a rendered plot
 shinyServer(function(input, output) {
   # Save a 'scatter' property which is a renderPlot object (that renders a scatterplot)
-  
+  output$value <- renderPrint({ input$num })
+  output$value <- renderPrint({ input$select })
 })
